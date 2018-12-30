681d89107650d2291a24cb6901ecd5c938f0e2a1
Gerard Rovira Sánchez <gerard.rovira.sanchez@gmail.com>
2018-12-30 21:24:08 +0100

    more verbose

--- src/app/bar.component.ts
+++ src/app/bar.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-bar',
   templateUrl: 'bar.component.html'
 })
-export class BarComponent implements OnInit {
+export class BarComponent implements OnInit, OnDestroy {
 
   constructor(private http: HttpClient) {}
 
   ngOnInit()  {
+    console.info('[Bar] Loaded');
     this.http.get('//localhost:3000').subscribe((val) => {
       console.info(`[Bar] Response: ${val}`);
     });
+  }
+
+  ngOnDestroy() {
+    console.info('[Bar] Destroyed');
   }
 }
--- src/app/foo.component.ts
+++ src/app/foo.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-foo',
   templateUrl: 'foo.component.html'
 })
-export class FooComponent implements OnInit {
+export class FooComponent implements OnInit, OnDestroy {
 
   constructor(private http: HttpClient) {}
 
   ngOnInit()  {
+    console.info('[Foo] Loaded');
     this.http.get('//localhost:3000').subscribe((val) => {
       console.info(`[Foo] Response: ${val}`);
     });
+  }
+
+  ngOnDestroy() {
+    console.info('[Foo] Destroyed');
   }
 }
