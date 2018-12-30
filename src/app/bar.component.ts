c9ef3b49456451575d4213437acf04d6ca7a3141
Gerard Rovira Sánchez <gerard.rovira.sanchez@gmail.com>
2018-12-30 21:29:23 +0100

    added good version

--- src/app/bar.component.ts
+++ src/app/bar.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-bar',
   templateUrl: 'bar.component.html'
 })
 export class BarComponent implements OnInit, OnDestroy {
+  subject = new Subject<void>();
 
   constructor(private http: HttpClient) {}
 
   ngOnInit()  {
     console.info('[Bar] Loaded');
     this.http.get('//localhost:3000').subscribe((val) => {
-      console.info(`[Bar] Response: ${val}`);
+      console.info(`[Bar-bad] Response: ${val}`);
+    });
+    this.http.get('//localhost:3000').pipe(
+      takeUntil(this.subject),
+    ).subscribe((val) => {
+      console.info(`[Bar-good] Response: ${val}`);
     });
   }
 
   ngOnDestroy() {
     console.info('[Bar] Destroyed');
+    this.subject.next();
+    this.subject.complete();
   }
 }
--- src/app/foo.component.ts
+++ src/app/foo.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-foo',
   templateUrl: 'foo.component.html'
 })
 export class FooComponent implements OnInit, OnDestroy {
+  subject = new Subject<void>();
 
   constructor(private http: HttpClient) {}
 
   ngOnInit()  {
     console.info('[Foo] Loaded');
     this.http.get('//localhost:3000').subscribe((val) => {
-      console.info(`[Foo] Response: ${val}`);
+      console.info(`[Foo-bad] Response: ${val}`);
+    });
+    this.http.get('//localhost:3000').pipe(
+      takeUntil(this.subject),
+    ).subscribe((val) => {
+      console.info(`[Foo-good] Response: ${val}`);
     });
   }
 
   ngOnDestroy() {
     console.info('[Foo] Destroyed');
+    this.subject.next();
+    this.subject.complete();
   }
 }
